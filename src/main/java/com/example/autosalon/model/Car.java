d16db4f8b46db1269102ac074fa6e4a036294c91
Aleksey Volkov <Java.merkor@gmail.com>
2021-12-24 18:19:43 +0300

    H2 database

--- src/main/java/com/example/autosalon/model/Car.java
+++ src/main/java/com/example/autosalon/model/Car.java
@@ -1,12 +1,10 @@
 package com.example.autosalon.model;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.util.Set;
+
 
 @Getter
 @Setter
