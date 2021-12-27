bcc25a5cd5f9136dd14624e5898e798608ebcfe2
Sergei Volkov <volkovself@gmail.com>
2021-12-27 20:11:30 +0300

    Добавление аннотаций к контроллеру для свагера

--- src/main/java/com/example/autosalon/controller/MainController.java
+++ src/main/java/com/example/autosalon/controller/MainController.java
@@ -1,6 +1,9 @@
 package com.example.autosalon.controller;
 import com.example.autosalon.model.Orders;
 import com.example.autosalon.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,7 +12,7 @@ import java.util.List;
 @RestController
 public class MainController {
 
-    private OrderService orderService;
+    private final OrderService orderService;
 
     @Autowired
     public MainController(OrderService orderService) {
@@ -21,6 +24,14 @@ public class MainController {
         return orderService.listOrders();
     }
 
+    @Operation(
+            summary = "Возвращает справочник стран",
+            description = "Возвращает справочник стран."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Справочник стран"),
+            @ApiResponse(responseCode = "500", description = "Внутренняя ошибка сервера")})
+    @GetMapping("/orders")
     @PostMapping("/sum")
     public int getRevenue(@RequestParam("start") String start,
                           @RequestParam("end") String end) {
